22324eb12de1e4d45deedc6e74076366bd852e2b
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-07-06 12:12:38 +0300

    feat: add settings styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import { Component } from 'react';
 import Search from './components/search/Searh';
 import ResultsBlock from './components/resultsBlock/ResultsBlock';
 
-import './App.css';
-
 class App extends Component {
   render() {
     return (
