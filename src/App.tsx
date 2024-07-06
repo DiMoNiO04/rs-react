4d744901f30342924d096c080ae3bd188e6e7608
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-07-06 12:00:16 +0300

    feat: add fonts and favicon and normalize.css

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import { Component } from 'react';
 import Search from './components/search/Searh';
 import ResultsBlock from './components/resultsBlock/ResultsBlock';
 
+import './App.css';
+
 class App extends Component {
   render() {
     return (
