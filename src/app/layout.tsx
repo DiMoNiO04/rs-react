4e4c1e6a853e151171149c2719f2a4607005d846
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-08-01 23:31:14 +0300

    feat: add next fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,12 @@
 import type { Metadata } from 'next';
+import { Nunito } from 'next/font/google';
+import './globals.css';
+
+const nunito = Nunito({
+  subsets: ['cyrillic'],
+  variable: '--font-nunito',
+  weight: ['400', '500', '600', '700', '800', '900'],
+});
 
 export const metadata: Metadata = {
   title: 'Star Wars Search',
@@ -8,7 +16,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className={nunito.className}>
         <div id="root">{children}</div>
       </body>
     </html>
